2b4c15e30ad0d73c75e1f6c7b8aafc23cac67aab
unknown <redX@redX-PC.(none)>
2012-04-20 18:02:00 -0400

    Login Phase 1

--- src/index.php
+++ src/index.php
@@ -1,27 +1,3 @@
 <?php
-	include("../lib/global.php");
-	if($_POST && $_POST['username'] && $_POST['password']){
-		$result = login($_POST['username'], $_POST['password']);
-		echo "login: " . $result;
-	}
+	include("login.php");
 ?>
-<!DOCTYPE html>
-<html >
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>SAPIENS Login</title>
-<link rel="stylesheet" type="text/css" href="../public/css/sapiens.css" />
-<link href='http://fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'></head>
-<body>
-<div id ="title">SAPI<span style="color:#4CE11C;">ENS</span></div>
-<div id="container">
-<div class="register">
-	<form action="login.php" method="post">
-		<label for="username">Username</label><input type="text" name="username"></br>
-		<label for="password">Password</label><input type="password" name="password"></br>
-		<input type="submit" name="login" value="login">
-	</form>
-</div>
-</div>
-</body>
-</html>
