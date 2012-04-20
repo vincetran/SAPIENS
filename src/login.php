5a9e6863b3ff1def20bcd299f50cd5adea02bf84
Vince Tran <hello@vincetran.me>
2012-04-20 15:05:41 -0400

    Reorganized files

--- src/login.php
+++ src/login.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>SAPIENS Login</title>
-<link rel="stylesheet" type="text/css" href="public/css/sapiens.css" />
+<link rel="stylesheet" type="text/css" href="../public/css/sapiens.css" />
 <link href='http://fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'></head>
 <body>
 <div id ="title">SAPI<span style="color:#4CE11C;">ENS</span></div>
