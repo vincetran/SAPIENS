0774adfc5a4dbd664c67e3a20c5dc37b5aab09ce
Sean Myers <seanmyers0608@gmail.com>
2012-04-20 13:51:09 -0400

    Added SAPIENS LOGIN

---
+++ public/headers.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Assignment 3</title>
+	<link rel="stylesheet" type="text/css" href="" />
+</head>
+<body>
+	<div class="header">
+
+	</div>
--- register.php
+++ register.php
@@ -1,33 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html >
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Assignment 3</title>
-<link rel="stylesheet" type="text/css" href="styles.css" />
-</head>
-<?php
-	include("global.php");
-
-?>
+<link rel="stylesheet" type="text/css" href="public/css/sapiens.css" />
+<link href='http://fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'></head>
 <body>
+<div id ="title">SAPI<span style="color:#4CE11C;">ENS</span></div>
 <div id="container">
-	<div id="center">
+<div class="register">
 	<form action="newuser.php" method="post">
-		Username:* <input type="text" name="username"></br>
-		Password:* <input type="password" name="password"></br>
-		Email:* <input type="text" name="email"></br>
-		First Name: <input type="text" name="fName"></br>
-		Last Name: <input type="text" name="lName"></br></br>
-		* = Required
-		<?php
-			if(isset($_POST['register']))
-			{
-				if(register($_POST['username'], $_POST['password'], $_POST['fName'], $_POST['lName'], $_POST['email']) == -1)
-				{
-					echo "<p class=\"error\">Username already exists. Please choose a new username.</p>";
-				}
-			}
-		?>
+		<label for="username">Username*</label><input type="text" name="username"></br>
+		<label for="password">Password*</label><input type="password" name="password"></br>
+		<label for="email">Email*</label><input type="text" name="email"></br>
+		<label for="fname">First Name</label><input type="text" name="fName"></br>
+		<label for="lname">Last Name</label><input type="text" name="lName"></br>
+		<label for="lname">Cell Phone Provider</label><select >
+		  <option value="volvo">Volvo</option>
+		  <option value="saab">Saab</option>
+		  <option value="mercedes">Mercedes</option>
+		  <option value="audi">Audi</option>
+		</select></br>
 		<input type="submit" name="register" value="Register">
 	</form>
 </div>
