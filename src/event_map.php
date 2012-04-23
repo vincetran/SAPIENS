e710df66dbf1082e8092b5c95ceed75647e620d6
Sean Myers <seanmyers0608@gmail.com>
2012-04-22 20:10:48 -0400

    FuCK YOU VINCE

--- src/event_map.php
+++ src/event_map.php
@@ -48,17 +48,21 @@ if(!User::resume()){
         });   
     }
     function placeEvent(data){
-        var location = new google.maps.LatLng(40.4412298,-79.95594);
+        var location = new google.maps.LatLng(data.lat,data.long);
         var mark = placeMarker(location);
-        var fullText= '<div class="scrollbar-container"><div class="inner">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra porta magna ut malesuada. Pellentesque sem nisi, volutpat nec dictum nec, aliquam in ipsum. Nullam ac libero ut justo pretium iaculis. Donec porta, mi quis scelerisque egestas, tortor risus sagittis enim, sit amet rutrum sapien orci quis nisi. Curabitur quis magna in neque tincidunt consectetur. Integer tempor fringilla magna et interdum. Vivamus quis erat at risus adipiscing feugiat nec non risus. Sed neque diam, dignissim et cursus volutpat, volutpat non nibh. Mauris eu nibh enim. Morbi eu tellus ligula, sed pharetra dui.Maecenas faucibus tempus laoreet. Sed cursus, ligula a sodales pharetra, arcu elit varius sem, ut tempus justo erat vitae nunc. Integer quis lacus ac nibh posuere pharetra. Morbi dapibus, arcu ut volutpat dignissim, turpis elit scelerisque sapien, sed bibendum neque mauris a odio. Praesent vel ipsum ac urna iaculis mollis. Quisque in sapien ipsum. Curabitur eget mattis risus. Praesent in purus justo. Nunc ac porta elit. Nulla ac augue non tortor cursus dignissim id sed odio.Donec tristique ipsum sed neque scelerisque quis aliquam lectus semper. Suspendisse et massa a diam aliquet laoreet eget at tellus. Fusce magna arcu, porttitor sit amet elementum eget, varius in magna. Suspendisse potenti. Proin vitae ipsum sed leo bibendum lacinia fermentum ut nibh. Quisque eget lacus massa, ac feugiat neque. Suspendisse potenti. Proin malesuada tristique ante, bibendum dictum metus interdum in. Mauris sed arcu non nibh facilisis porta. Quisque mi nibh, convallis vitae pharetra sit amet, venenatis et neque. Maecenas elementum rhoncus arcu at sodales.Proin ornare porta lorem, ac consectetur tellus venenatis auctor. Nulla auctor, augue non semper varius, enim nibh adipiscing enim, vitae ornare neque ante id velit. Sed nec ullamcorper enim. Nulla lorem est, rutrum id venenatis sed, viverra tincidunt magna. Proin at enim in ligula commodo dictum vel a nibh. Phasellus eget enim nisi, at accumsan erat. Donec sem tortor, ornare sed varius sed, tempus id libero. Nunc sit amet neque nisi, id vestibulum ligula. Fusce in varius nisl. Maecenas tristique, leo et tincidunt posuere, nisi leo fermentum ipsum, sed dignissim erat elit eu eros. Aenean quis nisi ut nulla posuere cursus. Aliquam porttitor porta enim ut egestas. Fusce ornare tempus consequat.Sed ut lacinia enim. Fusce a imperdiet mi. Sed eu lorem ac metus auctor interdum. Nulla ornare hendrerit auctor. Aliquam ultricies orci non purus cursus et consectetur sapien euismod. Aenean est mi, suscipit vitae mattis ullamcorper, feugiat ac purus. Sed libero massa, tempor ac tristique nec, euismod vitae ipsum. In vehicula pretium pharetra. Donec tincidunt interdum faucibus. Aliquam erat volutpat. Sed aliquam ornare tristique. Vestibulum at cursus sapien. Praesent eleifend euismod consectetur. Sed gravida orci in risus molestie iaculis eget eget purus.</div></div>';
+        var fullText= '<div class="scrollbar-container"><div class="inner">'+data.name+'</div></div>';
         setUpInfoPane(fullText, mark);
     }
     
     
   $(document).ready(function(){
                 initialize();
-                data = {'lat':40.4412298, 'long':-79.95494};
-                placeEvent(data)
+                $.post("bitch.php", function(data){
+                  var i = 0;
+                  for(i = 0; i < data.length; i++){
+                    placeEvent(data[i]);                 
+                  }
+                }, 'json')
                 
        }); 
     </script>
